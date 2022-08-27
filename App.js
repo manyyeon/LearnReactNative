53812a4aa3cdc555314100b492b2e9bc318a4390
manyyeon <udayeon@naver.com>
2022-08-27 22:05:50 +0900

    초기화

--- App.js
+++ App.js
@@ -2,29 +2,15 @@ import React, { useState } from "react";
 import { Button } from "react-native";
 import { StyleSheet } from "react-native";
 import { SafeAreaView, View, Text } from "react-native";
-import Box from "./components/Box";
-import Counter from "./components/Counter";
-import Greeting from "./components/Greeting";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const onIncrease = () => {
-    setCount(count + 1);
-  };
-  const onDecrease = () => {
-    setCount(count - 1);
-  };
   return (
     <SafeAreaView style={styles.full}>
-      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
+      <Text>TodoApp</Text>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  full: {
-    flex: 1,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
