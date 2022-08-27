a48c259df6c75d770cdbdb83ee6cf264c30b9ac7
manyyeon <udayeon@naver.com>
2022-08-27 21:57:20 +0900

    Counter 추가

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-native";
+import { StyleSheet } from "react-native";
 import { SafeAreaView, View, Text } from "react-native";
+import Box from "./components/Box";
+import Counter from "./components/Counter";
+import Greeting from "./components/Greeting";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const onIncrease = () => {
+    setCount(count + 1);
+  };
+  const onDecrease = () => {
+    setCount(count - 1);
+  };
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello React!</Text>
-      </View>
+    <SafeAreaView style={styles.full}>
+      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+  },
+});
 
 export default App;
